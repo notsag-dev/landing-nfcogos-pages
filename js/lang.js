6a4f286407b750e8a4e2b3a68f18d27fddf9351b
notsag-dev <a.gaston.gimenez@gmail.com>
2023-02-08 14:09:16 +0700

    Add verified page

---
+++ js/lang.js
@@ -0,0 +1,8 @@
+const lang = {
+  en: {
+    title: "Bringing transparency and control to your cannabis business",
+  },
+  es: {
+    title: "Transparencia y control para tu negocio de cannabis",
+  },
+};
